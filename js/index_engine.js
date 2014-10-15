f85bcb030a317b77d35d1614a2997e9b452f6e89
intheon <allobon@gmail.com>
2014-10-15 17:52:27 +0100

    shift

--- js/index_engine.js
+++ js/index_engine.js
@@ -5,6 +5,47 @@ $('#readerXML').change(function (){
 
 $(document).ready(function(){
 	checkIfLoggedIn();
+
+	$(".menu li").click(function(event){
+		drawContentPanel(event.currentTarget.id);
+	});
+
+	 $('ul.tabs').each(function(){
+	    // For each set of tabs, we want to keep track of
+	    // which tab is active and it's associated content
+	    var $active, $content, $links = $(this).find('a');
+
+	    // If the location.hash matches one of the links, use that as the active tab.
+	    // If no match is found, use the first link as the initial active tab.
+	    $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+	    $active.addClass('active');
+
+	    $content = $($active[0].hash);
+
+	    // Hide the remaining content
+	    $links.not($active).each(function () {
+	      $(this.hash).hide();
+	    });
+
+	    // Bind the click event handler
+	    $(this).on('click', 'a', function(e){
+	      // Make the old tab inactive.
+	      $active.removeClass('active');
+	      $content.hide();
+
+	      // Update the variables with the new link and content
+	      $active = $(this);
+	      $content = $(this.hash);
+
+	      // Make the tab active.
+	      $active.addClass('active');
+	      $content.show();
+
+	      // Prevent the anchor's default click action
+	      e.preventDefault();
+	    });
+	  });
+
 });
 
 function checkIfLoggedIn()
@@ -15,16 +56,73 @@ function checkIfLoggedIn()
 		showElement('#logInPrompt');
 		hideElement('#toolbar');
 		hideElement('#content');
+		hideElement('#sidePanel');
 	}
 	else
 	{
 		// logged in -- needs content
 		showElement('#toolbar');
 		showElement('#content');
+		showElement('#sidePanel');
 		hideElement('#logInPrompt');
 		addUsernameToToolbar();
 	}
 }
+
+var typesOfContentPane = {
+	paneOne: "readers_menu_content",
+	paneTwo: "editions_menu_content",
+	paneThree: "access_menu_content",
+	paneFour: "config_menu_content"
+};
+
+function drawContentPanel(whichTask)
+{
+	var c = $("#" + whichTask + "_content");
+
+	switch (whichTask)
+	{
+		case "readers_menu" : 
+			hideShit(c);
+			c.fadeIn();
+			break;
+		case "editions_menu" : 
+			hideShit(c);
+			c.fadeIn();
+			break;
+		case "access_menu" : 
+			hideShit(c);
+			c.fadeIn();
+			break;
+		case "config_menu" : 
+			hideShit(c);
+			c.fadeIn();
+			break;
+	}
+
+	function hideShit(c)
+	{
+		for (val in typesOfContentPane){
+			if (typesOfContentPane.hasOwnProperty(val))
+			{
+				if (typesOfContentPane[val] !== c)
+				{
+					var paneToHide = typesOfContentPane[val];
+					$("#" + paneToHide).fadeOut(600);
+				}
+			}
+		}
+	}
+
+}
+
+function addUsernameToToolbar()
+{
+	var adminName = localStorage.getItem("adminName");
+	$("#adminName").html(adminName);
+}
+
+
 
 function showElement(element)
 {
@@ -34,6 +132,8 @@ function hideElement(element)
 {
 	$(element).fadeOut();
 }
+
+
 
 function grabValues()
 {
@@ -46,13 +146,6 @@ function grabValues()
 			localStorage.setItem("restClientIsLoggedIn",true);
 		}
 	checkIfLoggedIn();
-}
-
-
-function addUsernameToToolbar()
-{
-	var adminName = localStorage.getItem("adminName");
-	$("#adminName").html(adminName);
 }
 
 function logMeOut()
